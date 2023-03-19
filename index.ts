44453d2dd43eb355d6b755f366e0569e6653da9d
Jeff Schomay <1825491+jschomay@users.noreply.github.com>
2023-03-19 19:58:21 -0400

    basic inworld token server

---
+++ index.ts
@@ -0,0 +1,34 @@
+import { InworldClient, InworldPacket } from "@inworld/nodejs-sdk";
+import cors from "cors";
+import express from "express";
+
+const PORT = 4000;
+
+if (!process.env.INWORLD_KEY) {
+  throw new Error("INWORLD_KEY env variable is required");
+}
+
+if (!process.env.INWORLD_SECRET) {
+  throw new Error("INWORLD_SECRET env variable is required");
+}
+
+const client = new InworldClient().setApiKey({
+  key: process.env.INWORLD_KEY!,
+  secret: process.env.INWORLD_SECRET!,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("../deadline/dist/"));
+
+app.get("/get_token", async (_, res) => {
+  const token = await client.generateSessionToken();
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(token);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
